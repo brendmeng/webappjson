c321cdb021e5ec8cbffca5e076e79f93e0966bfd
brendmeng <mengbrend7@gmail.com>
2022-02-16 17:08:46 -0500

    testing code

--- script.js
+++ script.js
@@ -5,32 +5,33 @@ fetch("data.json").then(d=>d.json()).then(data=> {
 
      const filtered = (data.Projects.find(Projects=>Projects.id=="Police"))
 
+    document.querySelector("main").innerHTML=`
+    <h1>Data successfully loaded!</h1>
+    <p>
+      Name: <strong>${data.About.name}</strong><br>
+      Number of Projects: <strong>${data.Projects.length}</strong> <br>
+      Link Test: <a href="${data.Contact.Resume}" target="_blank"><i class="fas fa-user-tie"></i> Resume</a> <br>
+      Link Test 2: <a href="${data.Contact.Linkedin}" target="_blank"
+      > <i class="fab fa-linkedin"></i> Linkedin</a >
+      <br>
 
+      <img 
+      src="${filtered.teaser}"
+      class="profile"
+      alt="Photo of Brend"
+      style="max-width: 80%"
+    />
 
-//     document.querySelector("main").innerHTML=`
-//     <h1>Data successfully loaded!</h1>
-//     <p>
-//       Name: <strong>${data.About.name}</strong><br>
-//       Number of Projects: <strong>${data.Projects.length}</strong> <br>
-//       Link Test: <a href="${data.Contact.Resume}" target="_blank"><i class="fas fa-user-tie"></i> Resume</a> <br>
-//       Image Test: <a href="${data.Contact.Linkedin}" target="_blank"
-//       > <i class="fab fa-linkedin"></i>Linkedin</a >
-//       <img
-//       id="covid"
-//       src="${data.About.dog}"
-//       alt="image of covid project"
-//       /> <br>
-
-//       <img 
-//       src="${filtered.teaser}"
-//       class="profile"
-//       alt="Photo of Brend"
-//       style="max-width: 80%"
-//     />
+    <img
+    id="covid"
+    src="${data.About.dog}"
+    alt="image of dog test"
+    />
    
 
-//     </p>  
-// `
+    </p>  
+`
+//filtering by project
 console.log(data.Projects[0].source)
 
 console.log(filtered)
@@ -39,46 +40,46 @@ console.log(filtered.tags)
     
     
 
-    renderNavBar()
+    // renderNavBar()
 });
 
 //CODE FOR NEXT WEEK LAB, can ignore. Thank you!
 
 //copy html
 
-function renderNavBar(){
-    const nav = document.querySelector("nav");
+// function renderNavBar(){
+//     const nav = document.querySelector("nav");
 
-    nav.innerHTML = `
+//     nav.innerHTML = `
     
     
-    `
-}
+//     `
+// }
 
-//passing about data to renderabout function
+// //passing about data to renderabout function
 
-function renderMainPage(data)
-{
-    const main = document.querySelector("main");
-  //  main.innterHTML = renderAbout(data.about);
-  main.innerHTML += renderProjects(data.projects);
-}
+// function renderMainPage(data)
+// {
+//     const main = document.querySelector("main");
+//   //  main.innterHTML = renderAbout(data.about);
+//   main.innerHTML += renderProjects(data.projects);
+// }
 
-//about = JSON, passed from renderMainPage
+// //about = JSON, passed from renderMainPage
 
 
-function renderProjects(projects)
-{
-    return `Projects
-    ${projects.map(project =>renderProjects(project)).join("")};
-    const page = projectID ==null?"main"
-    `
-}
-function renderAbout(about)
-{ 
-    return `
+// function renderProjects(projects)
+// {
+//     return `Projects
+//     ${projects.map(project =>renderProjects(project)).join("")};
+//     const page = projectID ==null?"main"
+//     `
+// }
+// function renderAbout(about)
+// { 
+//     return `
 
-    ${about.name}
-    `
+//     ${about.name}
+//     `
 
-}
+// }
