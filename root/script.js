cc7ab83c344297f4af3b43c9fe4d997ed1f7453a
brendmeng <mengbrend7@gmail.com>
2022-03-01 00:20:14 -0500

    please

--- root/script.js
+++ root/script.js
@@ -1,7 +1,7 @@
 //Code for this week to ensure JSON data is loaded:
-import renderNavBar from "../components/navbar.js"
-import renderMainPage from "../components/mainpage.js"
-import renderProjectPage from "../components/projectpage.js"
+import renderNavBar from "./components/navbar.js"
+import renderMainPage from "./components/mainpage.js"
+import renderProjectPage from "./components/projectpage.js"
 
 
 fetch("/components/data.json")
